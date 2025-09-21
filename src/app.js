d792fa0d38641a4e91e763e43549c7896be551dc
Yash Mahto <yashmahto244@gmail.com>
2025-09-21 17:43:44 +0530

    initial setup

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello, Acquisitions!');
+});
+
+export default app;
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import 'dotenv/config.js'
+import  './server.js'
+
