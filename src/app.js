1c9741342e1fb93e482964fa0d3438939b82ce23
Yash Mahto <yashmahto244@gmail.com>
2025-09-23 13:30:30 +0530

    add logger and middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
 import express from 'express';
+import logger from './config/logger.js';
+import helmet from "helmet";
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
 
 const app = express();
 
+app.use(helmet());
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
+
 app.get('/', (req, res) => {
+  logger.info('Hi!, Acquisitions-api');
   res.status(200).send('Hello, Acquisitions!');
 });
 
